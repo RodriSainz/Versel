a87f9f4f825bec0bc3b6e810c004029c85612b62
Rodrigo Sainz <tuemail@gmail.com>
2026-02-17 19:55:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { NavLink, Route, Routes } from "react-router";
+
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <h1>Nike</h1>
+      </nav>
+      
+    </div>
+  );
+}
+
+export default App;
